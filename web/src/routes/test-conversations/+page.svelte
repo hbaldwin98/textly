ad0891a1dc07e1c01eefd16631f2499448c96aec
Hunter Baldwin <huntercbaldwin98@gmail.com>
2025-06-01 15:18:53 -0700

    removed unused test-conversations file, updated docker files

--- web/src/routes/test-conversations/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import ConversationTest from '$lib/components/ai/ConversationTest.svelte';
-</script>
-
-<svelte:head>
-  <title>Conversation System Test - Textly</title>
-</svelte:head>
-
-<ConversationTest /> 
